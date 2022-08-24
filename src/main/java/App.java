c23a01c1b5ce8e001a96570a5ccbb27f311960b0
Alexandr-Minko <Minko.Alexandr@gmail.com>
2022-08-24 13:17:31 +0300

    Task completed

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,14 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean = applicationContext.getBean("helloworld", HelloWorld.class);
+        HelloWorld bean2 = applicationContext.getBean("helloworld", HelloWorld.class);
         System.out.println(bean.getMessage());
+
+        Cat catBean1 = applicationContext.getBean("cat", Cat.class);
+        Cat catBean2 = applicationContext.getBean("cat", Cat.class);
+
+        System.out.println("Бины HelloWorld равны по ссылке? " + (bean == bean2));
+        System.out.println("Бины Car равны по ссылке? " + (catBean1 == catBean2));
     }
 }
